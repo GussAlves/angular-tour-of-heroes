7e554c5cc362b5411166fafc15524c9941986946
Gusta Alves <gus.alves.araujo@gmail.com>
2021-06-07 13:48:38 -0300

    adjustments to the environment,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-tuor-of-heroes';
+  title = 'Tuor of heroes';
 }
