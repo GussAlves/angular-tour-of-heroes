712f22238da7c430c0fb1a4b5bb23e909e3a5023
Gusta Alves <gus.alves.araujo@gmail.com>
2021-06-07 21:02:09 -0300

    Add mock-heroes in /app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Iron-man' },
+    { id: 12, name: 'Spider-man' },
+    { id: 13, name: 'Magneto' },
+    { id: 14, name: 'Hulk' },
+    { id: 15, name: 'Black-Panter' },
+    { id: 16, name: 'Windstorm' },
+    { id: 17, name: 'Mister X' },
+    { id: 18, name: 'Batman' },
+    { id: 19, name: 'Superman' },
+    { id: 20, name: 'Superboy' }
+]
